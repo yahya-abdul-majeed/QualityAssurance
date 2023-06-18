e74b851394fa4f89aa6506c544f79487d5d65bac
YahyaAbdulMajeed <yahya.zf2@gmail.com>
2023-06-19 00:58:53 +0700

    Added UI tests.

---
+++ UI tests/AutomatedUITests.cs
@@ -0,0 +1,82 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using UI_tests.PageObjectModels;
+using Xunit.Sdk;
+
+namespace UI_tests
+{
+    public class AutomatedUITests : IDisposable
+    {
+        private readonly IWebDriver _driver;
+        private readonly HomePage _homePage;
+        public AutomatedUITests()
+        {
+            _driver = new ChromeDriver();
+            _homePage = new HomePage(_driver);
+            _homePage.Navigate();
+        }
+
+        [Fact]
+        public void Index_WhenExecuted_ReturnsIndexViewWithCorrectTitle()
+        {
+           Assert.Equal("Home Page - Web_App", _driver.Title);
+        }
+        [Fact]
+        public void Index_WhenExecuted_ReturnsIndexViewWithCorrectPrompt()
+        {
+            var prompt = "Given n pairs of parentheses, generate" +
+                " all combinations of well-formed parentheses.";
+            Assert.Equal(prompt, _homePage.PromptElement.Text);
+        }
+        [Fact]
+        public void Index_WhenExecuted_ReturnsIndexViewWithCorrectConstraints()
+        {
+            var constraints = "Constraints: 1 <= n <= 8";
+            Assert.Equal(constraints, _homePage.ConstraintsElement.Text);
+        }
+        [Fact]
+        public void Index_WhenExecuted_ReturnsIndexViewWithEmptyListOfCombinations()
+        {
+            var listOfCombinations = _homePage.Combinations.Select(e => e.Text).ToList();
+            Assert.Empty(listOfCombinations);
+        }
+
+        [Theory]
+        [InlineData(3)]
+        public void Index_WhenButtonClicked_ReturnsListOfCombinations_ForValidInput(int n)
+        {
+            _homePage.InputN(n);
+            _homePage.ClickBtn();
+            var listOfCombinations = _homePage.Combinations.Select(e => e.Text).ToList();
+            Assert.Equal(5, listOfCombinations.Count);
+        }
+        [Theory]
+        [InlineData(9)]
+        public void Index_InputBackgroundChangesRed_ForInvalidInput(int n)
+        {
+            _homePage.InputN(n);
+            var bgColor = _homePage.InputElement.GetCssValue("backgroundColor");
+            string[] rgba = bgColor.Replace("rgba(", "").Replace(")", "").Split(',');
+            int r = int.Parse(rgba[0].Trim());
+            int g = int.Parse(rgba[1].Trim());
+            int b = int.Parse(rgba[2].Trim());
+            string hexColor = $"#{r:X2}{g:X2}{b:X2}";
+            Assert.Equal("#FA5E71",hexColor);
+        }
+
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+    }
+}
---
+++ UI tests/PageObjectModels/HomePage.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI_tests.PageObjectModels
+{
+    public class HomePage
+    {
+        private readonly IWebDriver _driver;
+        private const string URI = "https://localhost:7173/";
+
+        By PageTitle = By.TagName("title");
+        By Input = By.Id("inputField");
+        By Button = By.Id("btnGenerate");
+        By Prompt = By.Id("Prompt");
+        By Constraints = By.Id("constraints");
+        By Combination = By.ClassName("combination");
+
+        public IWebElement InputElement => _driver.FindElement(Input);
+        public IWebElement ButtonElement => _driver.FindElement(Button);
+        public IWebElement PromptElement => _driver.FindElement(Prompt);
+        public IWebElement ConstraintsElement => _driver.FindElement(Constraints);
+        public ReadOnlyCollection<IWebElement> Combinations => _driver.FindElements(Combination);
+
+        public HomePage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+        public void Navigate() => _driver.Navigate().GoToUrl(URI);
+        public void InputN(int n) => InputElement.SendKeys(n.ToString());
+        public void ClickBtn()=> ButtonElement.Click();
+    }
+}
